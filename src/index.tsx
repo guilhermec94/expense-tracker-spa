546fa304fdedeba9569a3bf9474de4927477f007
Guilherme <guilhermewk94@gmail.com>
2023-09-27 10:21:39 +0100

    Initial commit
    Removes default react files and adds basic auth0 login and logout features

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
+import LoginButton from "./components/loginButton";
+import LogoutButton from "./components/logoutButton";
+import Profile from "./components/userProfile";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-bhd4s9vb.eu.auth0.com"
+      clientId="0VrQ6mUCNkEHoToiLB6VOtxPgPk0qyGz"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
